32b7602e136025b1d941e30589537ebe9b6d5738
Oliver Gray <oliver.gray@codasip.com>
2026-02-10 10:17:37 +0000

    [CHERI] Add support for RVY(0.9.3) in portASM.S
    Use previously defined macros to ensure portASM and porcontext.h
    can build and run for both RVY(0.9.3) and without breaking RVI.
    
    Most changes are simply register aliases, to ensure use of
    capability register, or capability aware instructions. The only
    more significant change is where registers were assigned value
    directly from a symbol, which is not valid in RVY. Instead the
    address must first be loaded with `la` (RVI) or `lgc` (RVY), then
    the required value can be loaded to the register.
    
    e.g
    
    ```
     load_x  sp, pxCurrentTCB            /* Load pxCurrentTCB. */
     load_x  sp, 0( sp )                 /* Read sp from first TCB member. */
    ```
    becomes:
    
    ```
     load_a  SP, pxCurrentTCB            /* Load pxCurrentTCB address. */
     load_x  SP, 0( SP )		     /* Load TCB pointer from pxCurrentTCB. */
     load_x  SP, 0( SP )                 /* Read sp from first TCB member. */
    ```
    
    Note: Support for FPU and VPU has not been tested, either in build or
    execution. Though in theory those sections should build and function as expected
    when enabled.

--- portable/GCC/RISC-V/portContext.h
+++ portable/GCC/RISC-V/portContext.h
@@ -230,186 +230,187 @@
 /*-----------------------------------------------------------*/
 
     .macro portcontexSAVE_FPU_CONTEXT
-addi sp, sp, -( portFPU_CONTEXT_SIZE )
+ADDI SP, SP, -( portFPU_CONTEXT_SIZE )
 /* Store the FPU registers. */
-store_f f0,  portFPU_REG_OFFSET( 0  )( sp )
-store_f f1,  portFPU_REG_OFFSET( 1  )( sp )
-store_f f2,  portFPU_REG_OFFSET( 2  )( sp )
-store_f f3,  portFPU_REG_OFFSET( 3  )( sp )
-store_f f4,  portFPU_REG_OFFSET( 4  )( sp )
-store_f f5,  portFPU_REG_OFFSET( 5  )( sp )
-store_f f6,  portFPU_REG_OFFSET( 6  )( sp )
-store_f f7,  portFPU_REG_OFFSET( 7  )( sp )
-store_f f8,  portFPU_REG_OFFSET( 8  )( sp )
-store_f f9,  portFPU_REG_OFFSET( 9  )( sp )
-store_f f10, portFPU_REG_OFFSET( 10 )( sp )
-store_f f11, portFPU_REG_OFFSET( 11 )( sp )
-store_f f12, portFPU_REG_OFFSET( 12 )( sp )
-store_f f13, portFPU_REG_OFFSET( 13 )( sp )
-store_f f14, portFPU_REG_OFFSET( 14 )( sp )
-store_f f15, portFPU_REG_OFFSET( 15 )( sp )
-store_f f16, portFPU_REG_OFFSET( 16 )( sp )
-store_f f17, portFPU_REG_OFFSET( 17 )( sp )
-store_f f18, portFPU_REG_OFFSET( 18 )( sp )
-store_f f19, portFPU_REG_OFFSET( 19 )( sp )
-store_f f20, portFPU_REG_OFFSET( 20 )( sp )
-store_f f21, portFPU_REG_OFFSET( 21 )( sp )
-store_f f22, portFPU_REG_OFFSET( 22 )( sp )
-store_f f23, portFPU_REG_OFFSET( 23 )( sp )
-store_f f24, portFPU_REG_OFFSET( 24 )( sp )
-store_f f25, portFPU_REG_OFFSET( 25 )( sp )
-store_f f26, portFPU_REG_OFFSET( 26 )( sp )
-store_f f27, portFPU_REG_OFFSET( 27 )( sp )
-store_f f28, portFPU_REG_OFFSET( 28 )( sp )
-store_f f29, portFPU_REG_OFFSET( 29 )( sp )
-store_f f30, portFPU_REG_OFFSET( 30 )( sp )
-store_f f31, portFPU_REG_OFFSET( 31 )( sp )
+store_f f0,  portFPU_REG_OFFSET( 0  )( SP )
+store_f f1,  portFPU_REG_OFFSET( 1  )( SP )
+store_f f2,  portFPU_REG_OFFSET( 2  )( SP )
+store_f f3,  portFPU_REG_OFFSET( 3  )( SP )
+store_f f4,  portFPU_REG_OFFSET( 4  )( SP )
+store_f f5,  portFPU_REG_OFFSET( 5  )( SP )
+store_f f6,  portFPU_REG_OFFSET( 6  )( SP )
+store_f f7,  portFPU_REG_OFFSET( 7  )( SP )
+store_f f8,  portFPU_REG_OFFSET( 8  )( SP )
+store_f f9,  portFPU_REG_OFFSET( 9  )( SP )
+store_f f10, portFPU_REG_OFFSET( 10 )( SP )
+store_f f11, portFPU_REG_OFFSET( 11 )( SP )
+store_f f12, portFPU_REG_OFFSET( 12 )( SP )
+store_f f13, portFPU_REG_OFFSET( 13 )( SP )
+store_f f14, portFPU_REG_OFFSET( 14 )( SP )
+store_f f15, portFPU_REG_OFFSET( 15 )( SP )
+store_f f16, portFPU_REG_OFFSET( 16 )( SP )
+store_f f17, portFPU_REG_OFFSET( 17 )( SP )
+store_f f18, portFPU_REG_OFFSET( 18 )( SP )
+store_f f19, portFPU_REG_OFFSET( 19 )( SP )
+store_f f20, portFPU_REG_OFFSET( 20 )( SP )
+store_f f21, portFPU_REG_OFFSET( 21 )( SP )
+store_f f22, portFPU_REG_OFFSET( 22 )( SP )
+store_f f23, portFPU_REG_OFFSET( 23 )( SP )
+store_f f24, portFPU_REG_OFFSET( 24 )( SP )
+store_f f25, portFPU_REG_OFFSET( 25 )( SP )
+store_f f26, portFPU_REG_OFFSET( 26 )( SP )
+store_f f27, portFPU_REG_OFFSET( 27 )( SP )
+store_f f28, portFPU_REG_OFFSET( 28 )( SP )
+store_f f29, portFPU_REG_OFFSET( 29 )( SP )
+store_f f30, portFPU_REG_OFFSET( 30 )( SP )
+store_f f31, portFPU_REG_OFFSET( 31 )( SP )
 csrr t0, fcsr
-store_x t0,  portFPU_REG_OFFSET( 32 )( sp )
+store_w t0,  portFPU_REG_OFFSET( 32 )( SP )
     .endm
 /*-----------------------------------------------------------*/
 
     .macro portcontextRESTORE_FPU_CONTEXT
 /* Restore the FPU registers. */
-load_f f0,  portFPU_REG_OFFSET( 0  )( sp )
-load_f f1,  portFPU_REG_OFFSET( 1  )( sp )
-load_f f2,  portFPU_REG_OFFSET( 2  )( sp )
-load_f f3,  portFPU_REG_OFFSET( 3  )( sp )
-load_f f4,  portFPU_REG_OFFSET( 4  )( sp )
-load_f f5,  portFPU_REG_OFFSET( 5  )( sp )
-load_f f6,  portFPU_REG_OFFSET( 6  )( sp )
-load_f f7,  portFPU_REG_OFFSET( 7  )( sp )
-load_f f8,  portFPU_REG_OFFSET( 8  )( sp )
-load_f f9,  portFPU_REG_OFFSET( 9  )( sp )
-load_f f10, portFPU_REG_OFFSET( 10 )( sp )
-load_f f11, portFPU_REG_OFFSET( 11 )( sp )
-load_f f12, portFPU_REG_OFFSET( 12 )( sp )
-load_f f13, portFPU_REG_OFFSET( 13 )( sp )
-load_f f14, portFPU_REG_OFFSET( 14 )( sp )
-load_f f15, portFPU_REG_OFFSET( 15 )( sp )
-load_f f16, portFPU_REG_OFFSET( 16 )( sp )
-load_f f17, portFPU_REG_OFFSET( 17 )( sp )
-load_f f18, portFPU_REG_OFFSET( 18 )( sp )
-load_f f19, portFPU_REG_OFFSET( 19 )( sp )
-load_f f20, portFPU_REG_OFFSET( 20 )( sp )
-load_f f21, portFPU_REG_OFFSET( 21 )( sp )
-load_f f22, portFPU_REG_OFFSET( 22 )( sp )
-load_f f23, portFPU_REG_OFFSET( 23 )( sp )
-load_f f24, portFPU_REG_OFFSET( 24 )( sp )
-load_f f25, portFPU_REG_OFFSET( 25 )( sp )
-load_f f26, portFPU_REG_OFFSET( 26 )( sp )
-load_f f27, portFPU_REG_OFFSET( 27 )( sp )
-load_f f28, portFPU_REG_OFFSET( 28 )( sp )
-load_f f29, portFPU_REG_OFFSET( 29 )( sp )
-load_f f30, portFPU_REG_OFFSET( 30 )( sp )
-load_f f31, portFPU_REG_OFFSET( 31 )( sp )
-load_x t0,  portFPU_REG_OFFSET( 32 )( sp )
+load_f f0,  portFPU_REG_OFFSET( 0  )( SP )
+load_f f1,  portFPU_REG_OFFSET( 1  )( SP )
+load_f f2,  portFPU_REG_OFFSET( 2  )( SP )
+load_f f3,  portFPU_REG_OFFSET( 3  )( SP )
+load_f f4,  portFPU_REG_OFFSET( 4  )( SP )
+load_f f5,  portFPU_REG_OFFSET( 5  )( SP )
+load_f f6,  portFPU_REG_OFFSET( 6  )( SP )
+load_f f7,  portFPU_REG_OFFSET( 7  )( SP )
+load_f f8,  portFPU_REG_OFFSET( 8  )( SP )
+load_f f9,  portFPU_REG_OFFSET( 9  )( SP )
+load_f f10, portFPU_REG_OFFSET( 10 )( SP )
+load_f f11, portFPU_REG_OFFSET( 11 )( SP )
+load_f f12, portFPU_REG_OFFSET( 12 )( SP )
+load_f f13, portFPU_REG_OFFSET( 13 )( SP )
+load_f f14, portFPU_REG_OFFSET( 14 )( SP )
+load_f f15, portFPU_REG_OFFSET( 15 )( SP )
+load_f f16, portFPU_REG_OFFSET( 16 )( SP )
+load_f f17, portFPU_REG_OFFSET( 17 )( SP )
+load_f f18, portFPU_REG_OFFSET( 18 )( SP )
+load_f f19, portFPU_REG_OFFSET( 19 )( SP )
+load_f f20, portFPU_REG_OFFSET( 20 )( SP )
+load_f f21, portFPU_REG_OFFSET( 21 )( SP )
+load_f f22, portFPU_REG_OFFSET( 22 )( SP )
+load_f f23, portFPU_REG_OFFSET( 23 )( SP )
+load_f f24, portFPU_REG_OFFSET( 24 )( SP )
+load_f f25, portFPU_REG_OFFSET( 25 )( SP )
+load_f f26, portFPU_REG_OFFSET( 26 )( SP )
+load_f f27, portFPU_REG_OFFSET( 27 )( SP )
+load_f f28, portFPU_REG_OFFSET( 28 )( SP )
+load_f f29, portFPU_REG_OFFSET( 29 )( SP )
+load_f f30, portFPU_REG_OFFSET( 30 )( SP )
+load_f f31, portFPU_REG_OFFSET( 31 )( SP )
+load_w t0,  portFPU_REG_OFFSET( 32 )( SP )
 csrw fcsr, t0
-addi sp, sp, ( portFPU_CONTEXT_SIZE )
+ADDI SP, SP, ( portFPU_CONTEXT_SIZE )
     .endm
 /*-----------------------------------------------------------*/
 
     .macro portcontexSAVE_VPU_CONTEXT
 /* Un-reserve the space reserved for mstatus and epc. */
-add sp, sp, ( 2 * portWORD_SIZE )
+ADD SP, SP, ( 2 * portWORD_SIZE )
 
 csrr t0, vlenb /* t0 = vlenb. vlenb is the length of each vector register in bytes. */
 slli t0, t0, 3 /* t0 = vlenb * 8. t0 now contains the space required to store 8 vector registers. */
 neg  t0, t0
 
 /* Store the vector registers in group of 8. */
-add     sp, sp, t0
-vs8r.v  v24, (sp)   /* Store v24-v31. */
-add     sp, sp, t0
-vs8r.v  v16, (sp)   /* Store v16-v23. */
-add     sp, sp, t0
-vs8r.v  v8, (sp)    /* Store v8-v15. */
-add     sp, sp, t0
-vs8r.v  v0, (sp)    /* Store v0-v7. */
+ADD     SP, SP, t0
+vs8r.v  v24, (SP)   /* Store v24-v31. */
+ADD     SP, SP, t0
+vs8r.v  v16, (SP)   /* Store v16-v23. */
+ADD     SP, SP, t0
+vs8r.v  v8, (SP)    /* Store v8-v15. */
+ADD     SP, SP, t0
+vs8r.v  v0, (SP)    /* Store v0-v7. */
 
 /* Store the VPU CSRs. */
-addi    sp, sp, -( 4 * portWORD_SIZE )
+ADDI    SP, SP, -( 4 * portWORD_SIZE )
 csrr    t0, vstart
-store_x t0, 0 * portWORD_SIZE( sp )
+store_x t0, 0 * portWORD_SIZE( SP )
 csrr    t0, vcsr
-store_x t0, 1 * portWORD_SIZE( sp )
+store_x t0, 1 * portWORD_SIZE( SP )
 csrr    t0, vl
-store_x t0, 2 * portWORD_SIZE( sp )
+store_x t0, 2 * portWORD_SIZE( SP )
 csrr    t0, vtype
-store_x t0, 3 * portWORD_SIZE( sp )
+store_x t0, 3 * portWORD_SIZE( SP )
 
 /* Re-reserve the space for mstatus and epc. */
-add sp, sp, -( 2 * portWORD_SIZE )
+ADD sp, sp, -( 2 * portWORD_SIZE )
     .endm
 /*-----------------------------------------------------------*/
 
     .macro portcontextRESTORE_VPU_CONTEXT
 /* Un-reserve the space reserved for mstatus and epc. */
-add sp, sp, ( 2 * portWORD_SIZE )
+ADD SP, SP, ( 2 * portWORD_SIZE )
 
 /* Restore the VPU CSRs. */
-load_x  t0, 0  * portWORD_SIZE( sp )
+load_x  t0, 0  * portWORD_SIZE( SP )
 csrw    vstart, t0
-load_x  t0, 1 * portWORD_SIZE( sp )
+load_x  t0, 1 * portWORD_SIZE( SP )
 csrw    vcsr, t0
-load_x  t0, 2 * portWORD_SIZE( sp )
-load_x  t1, 3 * portWORD_SIZE( sp )
+load_x  t0, 2 * portWORD_SIZE( SP )
+load_x  t1, 3 * portWORD_SIZE( SP )
 vsetvl  x0, t0, t1 /* vlen and vtype can only be updated by using vset*vl* instructions. */
-addi    sp, sp, ( 4 * portWORD_SIZE )
+ADDI    SP, SP, ( 4 * portWORD_SIZE )
 
 csrr t0, vlenb /* t0 = vlenb. vlenb is the length of each vector register in bytes. */
 slli t0, t0, 3 /* t0 = vlenb * 8. t0 now contains the space required to store 8 vector registers. */
 
 /* Restore the vector registers. */
-vl8r.v  v0, (sp)    /* Restore v0-v7. */
-add     sp, sp, t0
-vl8r.v  v8, (sp)    /* Restore v8-v15. */
-add     sp, sp, t0
-vl8r.v  v16, (sp)   /* Restore v16-v23. */
-add     sp, sp, t0
-vl8r.v  v24, (sp)   /* Restore v23-v31. */
-add     sp, sp, t0
+vl8r.v  v0, (SP)    /* Restore v0-v7. */
+ADD     SP, SP, t0
+vl8r.v  v8, (SP)    /* Restore v8-v15. */
+ADD     SP, SP, t0
+vl8r.v  v16, (SP)   /* Restore v16-v23. */
+ADD     SP, SP, t0
+vl8r.v  v24, (SP)   /* Restore v23-v31. */
+ADD     SP, SP, t0
 
 /* Re-reserve the space for mstatus and epc. */
-add sp, sp, -( 2 * portWORD_SIZE )
+ADD SP, SP, -( 2 * portWORD_SIZE )
     .endm
 /*-----------------------------------------------------------*/
 
    .macro portcontextSAVE_CONTEXT_INTERNAL
-addi sp, sp, -portCONTEXT_SIZE
-store_x x1,  2  * portWORD_SIZE( sp )
-store_x x5,  3  * portWORD_SIZE( sp )
-store_x x6,  4  * portWORD_SIZE( sp )
-store_x x7,  5  * portWORD_SIZE( sp )
-store_x x8,  6  * portWORD_SIZE( sp )
-store_x x9,  7  * portWORD_SIZE( sp )
-store_x x10, 8  * portWORD_SIZE( sp )
-store_x x11, 9  * portWORD_SIZE( sp )
-store_x x12, 10 * portWORD_SIZE( sp )
-store_x x13, 11 * portWORD_SIZE( sp )
-store_x x14, 12 * portWORD_SIZE( sp )
-store_x x15, 13 * portWORD_SIZE( sp )
+ADDI SP, SP, -portCONTEXT_SIZE
+store_x X1,  2  * portWORD_SIZE( SP )
+store_x X5,  3  * portWORD_SIZE( SP )
+store_x X6,  4  * portWORD_SIZE( SP )
+store_x X7,  5  * portWORD_SIZE( SP )
+store_x X8,  6  * portWORD_SIZE( SP )
+store_x X9,  7  * portWORD_SIZE( SP )
+store_x X10, 8  * portWORD_SIZE( SP )
+store_x X11, 9  * portWORD_SIZE( SP )
+store_x X12, 10 * portWORD_SIZE( SP )
+store_x X13, 11 * portWORD_SIZE( SP )
+store_x X14, 12 * portWORD_SIZE( SP )
+store_x X15, 13 * portWORD_SIZE( SP )
 #ifndef __riscv_32e
-    store_x x16, 14 * portWORD_SIZE( sp )
-    store_x x17, 15 * portWORD_SIZE( sp )
-    store_x x18, 16 * portWORD_SIZE( sp )
-    store_x x19, 17 * portWORD_SIZE( sp )
-    store_x x20, 18 * portWORD_SIZE( sp )
-    store_x x21, 19 * portWORD_SIZE( sp )
-    store_x x22, 20 * portWORD_SIZE( sp )
-    store_x x23, 21 * portWORD_SIZE( sp )
-    store_x x24, 22 * portWORD_SIZE( sp )
-    store_x x25, 23 * portWORD_SIZE( sp )
-    store_x x26, 24 * portWORD_SIZE( sp )
-    store_x x27, 25 * portWORD_SIZE( sp )
-    store_x x28, 26 * portWORD_SIZE( sp )
-    store_x x29, 27 * portWORD_SIZE( sp )
-    store_x x30, 28 * portWORD_SIZE( sp )
-    store_x x31, 29 * portWORD_SIZE( sp )
+    store_x X16, 14 * portWORD_SIZE( SP )
+    store_x X17, 15 * portWORD_SIZE( SP )
+    store_x X18, 16 * portWORD_SIZE( SP )
+    store_x X19, 17 * portWORD_SIZE( SP )
+    store_x X20, 18 * portWORD_SIZE( SP )
+    store_x X21, 19 * portWORD_SIZE( SP )
+    store_x X22, 20 * portWORD_SIZE( SP )
+    store_x X23, 21 * portWORD_SIZE( SP )
+    store_x X24, 22 * portWORD_SIZE( SP )
+    store_x X25, 23 * portWORD_SIZE( SP )
+    store_x X26, 24 * portWORD_SIZE( SP )
+    store_x X27, 25 * portWORD_SIZE( SP )
+    store_x X28, 26 * portWORD_SIZE( SP )
+    store_x X29, 27 * portWORD_SIZE( SP )
+    store_x X30, 28 * portWORD_SIZE( SP )
+    store_x X31, 29 * portWORD_SIZE( SP )
 #endif /* ifndef __riscv_32e */
 
-load_x t0, xCriticalNesting                                   /* Load the value of xCriticalNesting into t0. */
-store_x t0, portCRITICAL_NESTING_OFFSET * portWORD_SIZE( sp ) /* Store the critical nesting value to the stack. */
+load_a T0, xCriticalNesting                                   /* Load the address of xCriticalNesting into T0. */
+load_w t0, 0( T0 )                                            /* Load the value of xCriticalNesting into t0. */
+store_w t0, portCRITICAL_NESTING_OFFSET * portWORD_SIZE( SP ) /* Store the critical nesting value to the stack. */
 
 #if( configENABLE_FPU == 1 )
     csrr t0, mstatus
@@ -434,7 +435,7 @@ store_x t0, portCRITICAL_NESTING_OFFSET * portWORD_SIZE( sp ) /* Store the criti
 #endif
 
 csrr t0, mstatus
-store_x t0, 1 * portWORD_SIZE( sp )
+store_w t0, 1 * portWORD_SIZE( SP )
 
 portasmSAVE_ADDITIONAL_REGISTERS /* Defined in freertos_risc_v_chip_specific_extensions.h to save any registers unique to the RISC-V implementation. */
 
@@ -468,8 +469,9 @@ portasmSAVE_ADDITIONAL_REGISTERS /* Defined in freertos_risc_v_chip_specific_ext
 4:
 #endif
 
-load_x t0, pxCurrentTCB          /* Load pxCurrentTCB. */
-store_x sp, 0 ( t0 )             /* Write sp to first TCB member. */
+load_a T0, pxCurrentTCB          /* Load the address of pxCurrentTCB. */
+load_x T0, 0(T0)                 /* Load pxCurrentTCB. */
+store_x SP, 0 ( T0 )             /* Write sp to first TCB member. */
 
    .endm
 /*-----------------------------------------------------------*/
@@ -477,29 +479,32 @@ store_x sp, 0 ( t0 )             /* Write sp to first TCB member. */
    .macro portcontextSAVE_EXCEPTION_CONTEXT
 portcontextSAVE_CONTEXT_INTERNAL
 csrr a0, mcause
-csrr a1, mepc
-addi a1, a1, 4          /* Synchronous so update exception return address to the instruction after the instruction that generated the exception. */
-store_x a1, 0 ( sp )    /* Save updated exception return address. */
-load_x sp, xISRStackTop /* Switch to ISR stack. */
+csrr A1, MEPC
+ADDI A1, A1, 4          /* Synchronous so update exception return address to the instruction after the instruction that generated the exception. */
+store_x A1, 0 ( SP )    /* Save updated exception return address. */
+load_a SP, xISRStackTop /* Load the address of xISRStackTop. */
+load_x SP, 0 ( SP )     /* Switch to ISR stack. */
    .endm
 /*-----------------------------------------------------------*/
 
    .macro portcontextSAVE_INTERRUPT_CONTEXT
 portcontextSAVE_CONTEXT_INTERNAL
 csrr a0, mcause
-csrr a1, mepc
-store_x a1, 0 ( sp )    /* Asynchronous interrupt so save unmodified exception return address. */
-load_x sp, xISRStackTop /* Switch to ISR stack. */
+csrr A1, MEPC
+store_x A1, 0 ( SP )    /* Asynchronous interrupt so save unmodified exception return address. */
+load_a SP, xISRStackTop /* Load the address of xISRStackTop. */
+load_x SP, 0 ( SP )     /* Switch to ISR stack. */
    .endm
 /*-----------------------------------------------------------*/
 
    .macro portcontextRESTORE_CONTEXT
-load_x t1, pxCurrentTCB /* Load pxCurrentTCB. */
-load_x sp, 0 ( t1 )     /* Read sp from first TCB member. */
+load_a T1, pxCurrentTCB /* Load the address of pxCurrentTCB. */
+load_x T1, 0( T1 )      /* Load pxCurrentTCB. */
+load_x SP, 0( T1 )      /* Read sp from first TCB member. */
 
 /* Load mepc with the address of the instruction in the task to run next. */
-load_x t0, 0 ( sp )
-csrw mepc, t0
+load_x T0, 0 ( SP )
+csrw MEPC, T0
 
 /* Defined in freertos_risc_v_chip_specific_extensions.h to restore any registers unique to the RISC-V implementation. */
 portasmRESTORE_ADDITIONAL_REGISTERS
@@ -507,7 +512,7 @@ portasmRESTORE_ADDITIONAL_REGISTERS
 /* Restore mstatus register. It is important to use t3 (and not t0) here as t3
  * is not clobbered by portcontextRESTORE_VPU_CONTEXT and
  * portcontextRESTORE_FPU_CONTEXT. */
-load_x t3, 1 * portWORD_SIZE( sp )
+load_w t3, 1 * portWORD_SIZE( SP )
 csrw mstatus, t3
 
 #if( configENABLE_VPU == 1 )
@@ -530,41 +535,42 @@ csrw mstatus, t3
 6:
 #endif /* ifdef portasmSTORE_FPU_CONTEXT */
 
-load_x t0, portCRITICAL_NESTING_OFFSET * portWORD_SIZE( sp ) /* Obtain xCriticalNesting value for this task from task's stack. */
-load_x t1, pxCriticalNesting                                 /* Load the address of xCriticalNesting into t1. */
-store_x t0, 0 ( t1 )                                         /* Restore the critical nesting value for this task. */
+load_w t0, portCRITICAL_NESTING_OFFSET * portWORD_SIZE( SP ) /* Obtain xCriticalNesting value for this task from task's stack. */
+load_a T1, pxCriticalNesting                                 /* Load the address of pxCriticalNesting into t1. */
+load_x T1, 0( T1 )                                           /* Load the xCriticalNesting pointer (from *pxCriticalNesting) in to T1 */
+store_w t0, 0 ( T1 )                                         /* Restore the critical nesting value for this task to *xCriticalNesting. */
 
-load_x x1,  2  * portWORD_SIZE( sp )
-load_x x5,  3  * portWORD_SIZE( sp )
-load_x x6,  4  * portWORD_SIZE( sp )
-load_x x7,  5  * portWORD_SIZE( sp )
-load_x x8,  6  * portWORD_SIZE( sp )
-load_x x9,  7  * portWORD_SIZE( sp )
-load_x x10, 8  * portWORD_SIZE( sp )
-load_x x11, 9  * portWORD_SIZE( sp )
-load_x x12, 10 * portWORD_SIZE( sp )
-load_x x13, 11 * portWORD_SIZE( sp )
-load_x x14, 12 * portWORD_SIZE( sp )
-load_x x15, 13 * portWORD_SIZE( sp )
+load_x X1,  2  * portWORD_SIZE( SP )
+load_x X5,  3  * portWORD_SIZE( SP )
+load_x X6,  4  * portWORD_SIZE( SP )
+load_x X7,  5  * portWORD_SIZE( SP )
+load_x X8,  6  * portWORD_SIZE( SP )
+load_x X9,  7  * portWORD_SIZE( SP )
+load_x X10, 8  * portWORD_SIZE( SP )
+load_x X11, 9  * portWORD_SIZE( SP )
+load_x X12, 10 * portWORD_SIZE( SP )
+load_x X13, 11 * portWORD_SIZE( SP )
+load_x X14, 12 * portWORD_SIZE( SP )
+load_x X15, 13 * portWORD_SIZE( SP )
 #ifndef __riscv_32e
-    load_x x16, 14 * portWORD_SIZE( sp )
-    load_x x17, 15 * portWORD_SIZE( sp )
-    load_x x18, 16 * portWORD_SIZE( sp )
-    load_x x19, 17 * portWORD_SIZE( sp )
-    load_x x20, 18 * portWORD_SIZE( sp )
-    load_x x21, 19 * portWORD_SIZE( sp )
-    load_x x22, 20 * portWORD_SIZE( sp )
-    load_x x23, 21 * portWORD_SIZE( sp )
-    load_x x24, 22 * portWORD_SIZE( sp )
-    load_x x25, 23 * portWORD_SIZE( sp )
-    load_x x26, 24 * portWORD_SIZE( sp )
-    load_x x27, 25 * portWORD_SIZE( sp )
-    load_x x28, 26 * portWORD_SIZE( sp )
-    load_x x29, 27 * portWORD_SIZE( sp )
-    load_x x30, 28 * portWORD_SIZE( sp )
-    load_x x31, 29 * portWORD_SIZE( sp )
+    load_x X16, 14 * portWORD_SIZE( SP )
+    load_x X17, 15 * portWORD_SIZE( SP )
+    load_x X18, 16 * portWORD_SIZE( SP )
+    load_x X19, 17 * portWORD_SIZE( SP )
+    load_x X20, 18 * portWORD_SIZE( SP )
+    load_x X21, 19 * portWORD_SIZE( SP )
+    load_x X22, 20 * portWORD_SIZE( SP )
+    load_x X23, 21 * portWORD_SIZE( SP )
+    load_x X24, 22 * portWORD_SIZE( SP )
+    load_x X25, 23 * portWORD_SIZE( SP )
+    load_x X26, 24 * portWORD_SIZE( SP )
+    load_x X27, 25 * portWORD_SIZE( SP )
+    load_x X28, 26 * portWORD_SIZE( SP )
+    load_x X29, 27 * portWORD_SIZE( SP )
+    load_x X30, 28 * portWORD_SIZE( SP )
+    load_x X31, 29 * portWORD_SIZE( SP )
 #endif /* ifndef __riscv_32e */
-addi sp, sp, portCONTEXT_SIZE
+ADDI SP, SP, portCONTEXT_SIZE
 
 mret
    .endm
